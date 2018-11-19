b137cb13daabc8749b374e20c844e498507b955d
daimu <258141764@qq.com>
2018-11-19 17:16:02 +0800

    update

--- Script/UploadFile.js
+++ Script/UploadFile.js
@@ -9,21 +9,22 @@ var router = express.Router();
 router.post('/',function (req,res) {
     console.log(req.files[0]);  // 上传的文件信息
  
-    var des_file = "./upload/" + req.files[0].originalname;
-    fs.readFile( req.files[0].path, function (err, data) {
-        fs.writeFile(des_file, data, function (err) {
-            if( err ){
-                 console.log( err );
-            }else{
-                response = {
-                    message:'File uploaded successfully',
-                    filename:req.files[0].originalname
-                };
-                console.log( response );
-                res.end( JSON.stringify( response ) );
-            }
-        });
-    });
+    res.send('{result:success!}')
+    // var des_file = "./upload/" + req.files[0].originalname;
+    // fs.readFile( req.files[0].path, function (err, data) {
+    //     fs.writeFile(des_file, data, function (err) {
+    //         if( err ){
+    //              console.log( err );
+    //         }else{
+    //             response = {
+    //                 message:'File uploaded successfully',
+    //                 filename:req.files[0].originalname
+    //             };
+    //             console.log( response );
+    //             res.end( JSON.stringify( response ) );
+    //         }
+    //     });
+    // });
 })
  
 module.exports = router;
