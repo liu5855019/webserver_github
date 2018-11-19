78e92fe95ba19a5e5f15e0caffb5b3f4c8297914
daimu <258141764@qq.com>
2018-11-19 18:05:50 +0800

    update

--- Script/UploadFile.js
+++ Script/UploadFile.js
@@ -1,34 +1,57 @@
 
 
-var fs = require('fs');
-var express = require('express');
+// upload.js
 
- 
+var express = require('express');
 var router = express.Router();
 
-router.post('/',function (req,res) {
-    console.log(req);  // 上传的文件信息
- 
-    res.send('{result:success!}')
-    // var des_file = "./upload/" + req.files[0].originalname;
-    // fs.readFile( req.files[0].path, function (err, data) {
-    //     fs.writeFile(des_file, data, function (err) {
-    //         if( err ){
-    //              console.log( err );
-    //         }else{
-    //             response = {
-    //                 message:'File uploaded successfully',
-    //                 filename:req.files[0].originalname
-    //             };
-    //             console.log( response );
-    //             res.end( JSON.stringify( response ) );
-    //         }
-    //     });
-    // });
-})
- 
-module.exports = router;
+var fs = require('fs');
+var multer  = require('multer');
 
+// 使用硬盘存储模式设置存放接收到的文件的路径以及文件名
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        // 接收到文件后输出的保存路径（若不存在则需要创建）
+        cb(null, 'upload/');    
+    },
+    filename: function (req, file, cb) {
+        // 将保存文件名设置为 时间戳 + 文件原始名，比如 151342376785-123.jpg
+        cb(null, Date.now() + "-" + file.originalname);  
+    }
+});
+
+// 创建文件夹
+var createFolder = function(folder){
+    try{
+        // 测试 path 指定的文件或目录的用户权限,我们用来检测文件是否存在
+        // 如果文件路径不存在将会抛出错误"no such file or directory"
+        fs.accessSync(folder); 
+    }catch(e){
+        // 文件夹不存在，以同步的方式创建文件目录。
+        fs.mkdirSync(folder);
+    }  
+};
+
+var uploadFolder = './upload/';
+createFolder(uploadFolder);
+
+// 创建 multer 对象
+var upload = multer({ storage: storage });
+
+/* POST upload listing. */
+router.post('/', upload.any(), function(req, res, next) {
+    var files = req.files;
+    var file = files[0];
+    console.log(files);
+    // 接收文件成功后返回数据给前端
+    res.json({res_code: '0'});
+});
+
+
+
+
+// 导出模块（在 app.js 中引入）
+module.exports = router;
 
 
 
